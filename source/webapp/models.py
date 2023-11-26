4a20d2f8796c7c7e06776652ae5430129be457a7
User <kulgansad@gmail.com>
2023-11-26 13:49:15 +0600

    написал class Task

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    description = models.CharField(max_length=150, null=False, blank=False, verbose_name='Описание задачи')
+    status = models.CharField(max_length=50, default='new', verbose_name='Статус задачи')
+    date_of_completion = models.TextField(null=True, verbose_name='Дата выполнения YYYY-mm-dd')
+
+    def __str__(self):
+        return f'{self.id} {self.description}'
+
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index_page(request):
+    if request.method == 'GET':
+        return render(request, 'index.html')
