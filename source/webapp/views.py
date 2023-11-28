d882fb3f2aec756ae4e9f9ef1d990d264d92ef88
User <kulgansad@gmail.com>
2023-11-28 18:41:27 +0600

    изменил views.py

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -12,10 +12,16 @@ def create_task(request):
     if request.method == 'GET':
         return render(request, 'task.html', {'status_choices':status_choices})
     elif request.method == 'POST':
-        Task.objects.create(
-            description=request.POST.get('description'),
-            status=request.POST.get('status'),
-            date_of_completion=request.POST.get('date_of_completion')
-        )
+        if request.POST.get('date_of_completion') == "":
+            Task.objects.create(
+                description=request.POST.get('description'),
+                status=request.POST.get('status'),
+            )
+        else:
+            Task.objects.create(
+                description=request.POST.get('description'),
+                status=request.POST.get('status'),
+                date_of_completion=request.POST.get('date_of_completion')
+            )
         return HttpResponseRedirect('/')
 
